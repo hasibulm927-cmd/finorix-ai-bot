df360d540eb4fc1ceee71e7ba39b292d73bdd9ec
hasibulm927-cmd <hasibulm927@gmail.com>
2026-06-10 10:50:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,23 @@
+
 import asyncio
 from strategy import get_signal
 from telegram_bot import send_signal
 
 async def main():
 
+    print("Bot Started")
+
     signal = get_signal()
 
+    print("Signal =", signal)
+
     if signal == "BUY":
-        await send_signal(
-            "🟢 BUY SIGNAL\nPair: EURUSD\nEntry: Next Candle\nExpiry: 2 Minute"
-        )
+        await send_signal("BUY SIGNAL")
+        print("BUY sent")
 
     elif signal == "SELL":
-        await send_signal(
-            "🔴 SELL SIGNAL\nPair: EURUSD\nEntry: Next Candle\nExpiry: 2 Minute"
-        )
+        await send_signal("SELL SIGNAL")
+        print("SELL sent")
 
     else:
         print("WAIT")
