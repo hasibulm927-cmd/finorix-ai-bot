a90a0451cb30c467faf99d920ecda9ee70892ed5
hasibulm927-cmd <hasibulm927@gmail.com>
2026-06-12 19:49:56 +0530

    Update strategy.py

--- strategy.py
+++ strategy.py
@@ -2,6 +2,7 @@ import yfinance as yf
 from ta.trend import EMAIndicator, MACD
 from ta.momentum import RSIIndicator
 
+
 def check_pair(symbol):
 
     data = yf.download(
@@ -16,8 +17,6 @@ def check_pair(symbol):
         return None
 
     close = data["Close"].squeeze()
-    high = data["High"].squeeze()
-    low = data["Low"].squeeze()
 
     ema9 = EMAIndicator(close=close, window=9).ema_indicator()
     ema21 = EMAIndicator(close=close, window=21).ema_indicator()
@@ -25,8 +24,6 @@ def check_pair(symbol):
     rsi = RSIIndicator(close=close, window=14).rsi()
 
     macd = MACD(close=close)
-
-    latest_price = float(close.iloc[-1])
 
     latest_ema9 = float(ema9.iloc[-1])
     latest_ema21 = float(ema21.iloc[-1])
@@ -36,28 +33,19 @@ def check_pair(symbol):
     latest_macd = float(macd.macd().iloc[-1])
     latest_signal = float(macd.macd_signal().iloc[-1])
 
-    recent_high = float(high.tail(20).max())
-    recent_low = float(low.tail(20).min())
-
-    uptrend = latest_ema21 > float(ema21.iloc[-5])
-
-    # BUY
+    # BUY SIGNAL
     if (
         latest_ema9 > latest_ema21
-        and latest_rsi > 55
+        and latest_rsi > 50
         and latest_macd > latest_signal
-        and uptrend
-        and latest_price < recent_high * 0.995
     ):
         return ("BUY", symbol)
 
-    # SELL
+    # SELL SIGNAL
     elif (
         latest_ema9 < latest_ema21
-        and latest_rsi < 45
+        and latest_rsi < 50
         and latest_macd < latest_signal
-        and not uptrend
-        and latest_price > recent_low * 1.005
     ):
         return ("SELL", symbol)
 
@@ -72,7 +60,11 @@ def get_signal():
             "EURUSD=X",
             "GBPUSD=X",
             "AUDUSD=X",
-            "USDJPY=X"
+            "USDJPY=X",
+            "USDCAD=X",
+            "USDCHF=X",
+            "NZDUSD=X",
+            "EURJPY=X"
         ]
 
         for pair in pairs:
