4d17eb9206b9e8813b6cb2265c49c1b2898e372a
hasibulm927-cmd <hasibulm927@gmail.com>
2026-06-12 10:34:51 +0530

    Update strategy.py

--- strategy.py
+++ strategy.py
@@ -3,54 +3,72 @@ from ta.trend import EMAIndicator, MACD
 from ta.momentum import RSIIndicator
 
 
+def check_pair(symbol):
+
+    data = yf.download(
+        symbol,
+        period="5d",
+        interval="5m",
+        progress=False,
+        auto_adjust=True
+    )
+
+    if data.empty:
+        return None
+
+    close = data["Close"].squeeze()
+
+    ema9 = EMAIndicator(close=close, window=9).ema_indicator()
+    ema21 = EMAIndicator(close=close, window=21).ema_indicator()
+
+    rsi = RSIIndicator(close=close, window=14).rsi()
+
+    macd = MACD(close=close)
+
+    latest_ema9 = float(ema9.iloc[-1])
+    latest_ema21 = float(ema21.iloc[-1])
+
+    latest_rsi = float(rsi.iloc[-1])
+
+    latest_macd = float(macd.macd().iloc[-1])
+    latest_signal = float(macd.macd_signal().iloc[-1])
+
+    # BUY
+    if (
+        latest_ema9 > latest_ema21
+        and latest_rsi > 50
+        and latest_macd > latest_signal
+    ):
+        return ("BUY", symbol)
+
+    # SELL
+    elif (
+        latest_ema9 < latest_ema21
+        and latest_rsi < 50
+        and latest_macd < latest_signal
+    ):
+        return ("SELL", symbol)
+
+    return None
+
+
 def get_signal():
 
     try:
 
-        data = yf.download(
+        pairs = [
             "EURUSD=X",
-            period="5d",
-            interval="5m",
-            progress=False,
-            auto_adjust=True
-        )
+            "GBPUSD=X"
+        ]
 
-        if data.empty:
-            return "WAIT"
+        for pair in pairs:
 
-        close = data["Close"].squeeze()
+            result = check_pair(pair)
 
-        ema9 = EMAIndicator(close=close, window=9).ema_indicator()
-        ema21 = EMAIndicator(close=close, window=21).ema_indicator()
+            if result is not None:
+                return result
 
-        rsi = RSIIndicator(close=close, window=14).rsi()
-
-        macd = MACD(close=close)
-
-        latest_ema9 = float(ema9.iloc[-1])
-        latest_ema21 = float(ema21.iloc[-1])
-
-        latest_rsi = float(rsi.iloc[-1])
-
-        latest_macd = float(macd.macd().iloc[-1])
-        latest_signal = float(macd.macd_signal().iloc[-1])
-
-        if (
-            latest_ema9 > latest_ema21
-            and latest_rsi > 55
-            and latest_macd > latest_signal
-        ):
-            return "BUY"
-
-        elif (
-            latest_ema9 < latest_ema21
-            and latest_rsi < 45
-            and latest_macd < latest_signal
-        ):
-            return "SELL"
-
-        else:
-            return "WAIT"
+        return None
 
     except Exception as e:
         print("ERROR:", e)
